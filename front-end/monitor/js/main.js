beb400e77d930fd54dae979946e849863f5a76e9
Kitsune8812 <134330169+Kitsune8812@users.noreply.github.com>
2024-05-16 11:42:21 -0600

    monitor de iot car

---
+++ front-end/monitor/js/main.js
@@ -0,0 +1,40 @@
+
+let respuesta = document.getElementById('respuesta');
+function callApiRequest() {
+    axios
+    .get('http://localhost/iot-car-control/back-end/apis/getRegistro.php')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+
+let respuestaServidor =
+ response.data== 'w'?'adelante': 
+response.data=='s'?'atras':
+ response.data== 'a'?'izquierda':
+ response.data=='d'?'derecha': 'detener';
+
+
+    console.log(respuestaServidor);
+
+
+
+    respuesta.innerHTML = "Respuesta: <strong> " +response.data +  "</strong>";
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+    
+}
+
+function miFuncion() {
+    console.log("¡Hola! Esta función se ejecutará cada dos minutos.");
+    // Aquí puedes colocar el código que quieres que se ejecute cada dos minutos
+  }
+
+  // Ejecuta la función cada dos minutos (120,000 milisegundos)
+  setInterval(callApiRequest, 2000);
